3ef014cded3c7dad7221eb758d94c7ee9e29c186
liza <lizaessien23@gmail.com>
2022-01-21 00:02:34 +0200

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+function About() {
+    return (
+      <>
+        <h1 className='text-6xl mb-4'>Github Finder</h1>
+        <p className='mb-4 text-2xl font-light'>
+          This is a github finder app created with React and Tailwind css
+        </p>
+      </>
+    )
+  }
+  
+  export default About
